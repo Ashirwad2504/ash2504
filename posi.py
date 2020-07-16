91fbdf8b80d303f519cbaa888eda73ea30f9b11f
Ashirwad2504 <68374208+Ashirwad2504@users.noreply.github.com>
2020-07-16 20:53:39 +0530

    Add files via upload

---
+++ posi.py
@@ -0,0 +1,14 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=[-1,-2,3,4]
+>>> b=[]
+>>> for i in a:
+	if i>0:
+		b.append(i)
+
+		
+>>> 
+>>> 
+=== RESTART: C:/Users/dell/AppData/Local/Programs/Python/Python38/positive.py ==
+[3, 4]
+>>> 
